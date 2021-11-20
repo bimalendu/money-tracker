c704121ef1b34d987b632928e14b7bf469d78496
Bimalendu Behera <bimalendu.behera@gmail.com>
2021-11-20 21:01:30 +0530

    Disabled Teams and API token features

--- web/config/jetstream.php
+++ web/config/jetstream.php
@@ -44,9 +44,10 @@ return [
     'features' => [
         Features::termsAndPrivacyPolicy(),
         Features::profilePhotos(),
-        Features::api(),
-        Features::teams(['invitations' => true]),
         Features::accountDeletion(),
+        /*Features::api(),
+        Features::teams(['invitations' => true]),*/
+        
     ],
 
     /*
