31970db377800b70ad6ec82720825306b134c875
Bimalendu Behera <bimalendu.behera@gmail.com>
2021-12-02 23:59:19 +0530

    Implementing Graph

---
+++ web/app/Http/Livewire/Dashboard.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Expense;
+
+class Dashboard extends Component
+{
+   public ExpenseModel $expense;
+
+    public function render()
+    {
+        $expenses = Expense::where('user_id', auth()->user()->id)->get()->toArray();
+        $data = [];
+        foreach($expenses as $expense){
+            $record = [];
+            $record['date'] = $expense['on_date'];
+            $record['amount'] = $expense['price']; 
+
+            $data[] = $record;
+        }
+        
+        return view('dashboard',[
+            "expenses" => json_encode($data),
+        ]);
+    }
+}
